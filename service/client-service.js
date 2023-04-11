c8a34e8bae41747984c3f46661b4f5ec75730d8a
thiseb <boricuabamp@hotmail.com>
2023-04-11 02:07:54 -0500

    proyecto crud finalizado

---
+++ service/client-service.js
@@ -0,0 +1,45 @@
+const listaClientes = () => 
+fetch('http://localhost:3000/perfil').then( repuesta => repuesta.json());
+
+const crearCliente = (nombre, email) => {
+    return fetch('http://localhost:3000/perfil', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({nombre, email, id: uuid.v4()})
+    });
+};
+
+const eliminarCliente = (id) => {
+    return fetch(`http://localhost:3000/perfil/${id}`, {
+        method:'DELETE',
+    });
+};
+
+const detalleCliente = (id) => {
+    return fetch(`http://localhost:3000/perfil/${id}`).then(respuesta => 
+    respuesta.json()
+    );
+};
+
+const actualizarCliente = (nombre, email, id) => {
+    return fetch(`http://localhost:3000/perfil/${id}`, {
+        method:'PUT',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({nombre, email})
+    })
+    .then(respuesta => respuesta)
+    .catch(err => console.log(err));
+};
+
+
+export const clientServices = {
+    listaClientes,
+    crearCliente,
+    eliminarCliente,
+    detalleCliente,
+    actualizarCliente,
+};
